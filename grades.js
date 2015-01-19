b5fd1be7018c84e55581de7c6dd5258cf3183237
Ryan <rpw10104@saintjoe.edu>
2015-01-19 13:43:24 -0500

    basic outline of what to do next

---
+++ grades.js
@@ -0,0 +1,7 @@
+//Ryan Postma CMP 344
+//Im doing the grades problem. 
+//Have to create a grades object to store a set of student grades in an object
+//make a function to add a grade
+//make a function that will display the average
+var bam = "I Am Awesome!";
+print (bam);
