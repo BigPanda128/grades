c9007743af19e34d921b8af859810f201b16b1d1
Ryan Postma <rpostma@sjccs142.saintjoe.edu>
2015-01-21 19:23:25 -0500

    Ryan Postma chapter 2 grades exercise for CMP344

--- grades.js
+++ grades.js
@@ -3,5 +3,40 @@
 //Have to create a grades object to store a set of student grades in an object
 //make a function to add a grade
 //make a function that will display the average
-var bam = "I Am Awesome!";
-print (bam);
+
+
+//the student object
+function studentGrades() {
+	this.grades = [];
+	this.add = add;
+	this.average = average;
+}//end student object
+
+
+//function to add a grade into the array
+function add(grade) {
+	this.grades.push(grade);
+}//end add function
+
+
+//average function for students grades
+function average() {
+	var total = 0;
+	for (var i = 0; i < this.grades.length; ++i) {
+		total += this.grades[i];
+	}//end for
+	return print(total / this.grades.length);
+}//end average function
+
+
+var student1 = new studentGrades();
+
+student1.add(89);
+student1.add(93);
+student1.add(79);
+student1.add(97);
+student1.add(89);
+
+print(student1.grades);
+print("Student 1 average grades");
+student1.average();
